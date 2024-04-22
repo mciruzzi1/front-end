bcb18c252699a820d22baf536814a472903db6c6
Chris Comes <qx56jx35@gmail.com>
2024-04-22 00:32:29 -0400

    main

--- src/App.js
+++ src/App.js
@@ -3,28 +3,65 @@ import './App.css';
 import ReactModal from 'react-modal';
 
 function App() {
+  const NUM_DETECTORS = {
+    16: 43,
+    32: 83,
+    64: 161,
+    128: 321,
+    256: 639,
+    512: 1273,
+    1024: 2543,
+    2048: 5085,
+    4096: 10167
+  };
+  
   const [inputs, setInputs] = useState({
+    size: '',
+    distance: '',
+    detectors: '',
     views: '',
-    beams: '',
-    scanResolution: '',
-    sourceDistance: '',
-    detectorDistance: '',
-    detectorWidth: '',
+    iterations: ''
   });
 
   const [methods, setMethods] = useState({
     mlcm: false,
-    astra: false,
-    mirt: false,
+    astra_cpu: false,
+    astra_gpu: false,
     tigre: false,
   });
 
   const [results, setResults] = useState({
-    mlcmTime: '',
-    astraTime: '',
-    mirtTime: '',
-    tigreTime: '',
+    mlcm_time: '',
+    astra_cpu_time: '',
+    astra_gpu_time: '',
+    tigre_time: '',
   });
+
+  const [statusData, setStatusData] = useState({ running: false, completed: false, message: [] });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:8000/status');
+        const data = await response.json();
+
+        if (data["completed"] === true) {
+          fetch('http://localhost:8000/results').then((response) => response.json()).then((data) => {
+          setResults(r => ({...r, "mlcm_time": data["custom_gpu_avg"], "astra_cpu_time": data["astra_cpu_avg"], "astra_gpu_time": data["astra_gpu_avg"], "tigre_time": data["tigre_gpu_avg"]}));
+        })
+        }
+
+        setStatusData(s => ({...s, "running": data["running"], "completed": data["completed"], "message": data["message"]}));
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        // Optionally display an error message to the user
+      }
+    };
+
+    const intervalId = setInterval(fetchData, 200); // Update every 100 milliseconds (approximately 10 times per second)
+
+    return () => clearInterval(intervalId); // Cleanup function to clear the interval on unmount
+  }, []);
 
   //Set the state for the about section
   const [isOpen, setIsOpen] = useState(false);
@@ -39,7 +76,16 @@ function App() {
   //Change the data values as the user updates them
   const handleDataInput = (e) => {
     const { name, value } = e.target;
-    setInputs({ ...inputs, [name]: value });
+    
+    if (name === "size" && value in NUM_DETECTORS) {
+      setInputs({ ...inputs, [name]: value, "distance": value * 2, "detectors": NUM_DETECTORS[value] });
+    }
+    else if (name === "size") {
+      setInputs({ ...inputs, [name]: value, "distance": value * 2 });
+    }
+    else {
+      setInputs({ ...inputs, [name]: value});
+    }
   };
 
   //Change the methods used as the user updates them
@@ -50,40 +96,72 @@ function App() {
 
   const handleCalculate = (e) => {
     e.preventDefault();
-    // call the api and update results here
+
+    fetch('http://localhost:8000/run', {method:"POST", body: JSON.stringify({
+      "size": inputs.size,
+      "distance": inputs.distance,
+      "detectors": inputs.detectors,
+      "iterations": inputs.iterations,
+      "views": inputs.views,
+      "items": {
+        "astra_cpu": methods.astra_cpu,
+        "astra_gpu": methods.astra_gpu,
+        "tigre_gpu": methods.tigre,
+        "custom_gpu": methods.mlcm
+      },
+      }),headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+     },
+    })
   };
 
   //Set each value back to its original state
   const handleReset = (e) =>{
+    fetch('http://localhost:8000/reset', {method:"POST"})
+    
     setInputs({
+      size: '',
+      distance: '',
+      detectors: '',
       views: '',
-      beams: '',
-      scanResolution: '',
-      sourceDistance: '',
-      detectorDistance: '',
-      detectorWidth: '',
+      iterations: '',
     });
 
     setMethods({
+      astra_cpu: false,
+      astra_gpu: false,
       mlcm: false,
-      astra: false,
-      mirt: false,
       tigre: false,
     });
+
+    setStatusData({
+      running: false,
+      completed: false,
+      message: []
+    })
+
+    setResults({
+      astra_cpu_time: 0.0,
+      astra_gpu_time: 0.0,
+      mlcm_time: 0.0,
+      tigre_time: 0.0
+    })
   }
 
   return (
     <div className="app">
+      <h1>MLCM Senior Project</h1>
+      <h2>Fanbeam Computed Tomography Forward Model in OpenGL</h2>
       <div className="user-input">
         <div className="data-input">
-          <form className = "data-form" onSubmit={handleCalculate}>
+          <form className = "variable-form" onSubmit={handleSubmit}>
             <label>
               Views: 
               <input
                 type="number"
                 name="views"
                 value={inputs.views}
-                onChange={handleDataInput}
+                onChange={handleChange}
               />
               <button className='help-btn'
               onMouseEnter={() => setHelpButtonHoverV(true)}
@@ -94,12 +172,12 @@ function App() {
               {helpButtonHoverV && <div className="tooltip">This modifies the number of views used</div>}
             </label>
             <label>
-              Beams: 
+              Source & Detector Distance: 
               <input
                 type="number"
                 name="beams"
                 value={inputs.beams}
-                onChange={handleDataInput}
+                onChange={handleChange}
               />
               <button className='help-btn'
               onMouseEnter={() => setHelpButtonHoverB(true)}
@@ -110,12 +188,12 @@ function App() {
               {helpButtonHoverB && <div className="tooltip">This modifies the number of beams produced</div>}
             </label>
             <label>
-              Scan Resolution: 
+              Number of Detectors: 
               <input
                 type="number"
                 name="scanResolution"
                 value={inputs.scanResolution}
-                onChange={handleDataInput}
+                onChange={handleChange}
               />
               <button className='help-btn'
               onMouseEnter={() => setHelpButtonHoverR(true)}
@@ -126,12 +204,12 @@ function App() {
               {helpButtonHoverR && <div className="tooltip">This modifies the resolution of the image</div>}
             </label>
             <label>
-              Source Distance: 
+              Number of Views Around Image: 
               <input
                 type="number"
                 name="sourceDistance"
                 value={inputs.sourceDistance}
-                onChange={handleDataInput}
+                onChange={handleChange}
               />
               <button className='help-btn'
               onMouseEnter={() => setHelpButtonHoverSD(true)}
@@ -142,20 +220,13 @@ function App() {
               {helpButtonHoverSD && <div className="tooltip">This modifies the distance to the source</div>}
             </label>
             <label>
-              Detector Distance: 
+              Number of Iterations (average calculation): 
               <input
                 type="number"
                 name="detectorDistance"
                 value={inputs.detectorDistance}
-                onChange={handleDataInput}
+                onChange={handleChange}
               />
-              <button className='help-btn'
-              onMouseEnter={() => setHelpButtonHoverDD(true)}
-              onMouseLeave={() => setHelpButtonHoverDD(false)}
-              >
-                ?
-              </button>
-              {helpButtonHoverDD && <div className="tooltip">This modifies the distance between each detector</div>}
             </label>
             <label>
               Detector Width: 
@@ -163,7 +234,7 @@ function App() {
                 type="number"
                 name="detectorWidth"
                 value={inputs.detectorWidth}
-                onChange={handleDataInput}
+                onChange={handleChange}
               />
               <button className='help-btn'
               onMouseEnter={() => setHelpButtonHoverDW(true)}
@@ -176,8 +247,9 @@ function App() {
           </form>
         </div>
         <div className="technique-input">
+          <b>Select Algorithm Type</b>
           <label>
-            MLCM Rasterization Algorithim:
+            MLCM Custom Rasterization Algorithim:
             <input
               type="checkbox"
               name="mlcm"
@@ -186,21 +258,21 @@ function App() {
             />
           </label>
           <label>
-            ASTRA Toolkit:
+            ASTRA CPU (not recommend sizes&gt;512):
             <input
               type="checkbox"
               name="astra"
               checked={methods.astra}
-              onChange={handleMethodInput}
+              onChange={handleCheckboxChange}
             />
           </label>
           <label>
-            MIRT:
+            ASTRA GPU:
             <input
               type="checkbox"
               name="mirt"
               checked={methods.mirt}
-              onChange={handleMethodInput}
+              onChange={handleCheckboxChange}
             />
           </label>
           <label>
@@ -214,11 +286,36 @@ function App() {
           </label>
         </div>
       </div>
+      <div>
+        <h2>Status:</h2>
+        {statusData.running ? (
+          <p>Running!</p>
+        ) : (
+          <p>Not running.</p>
+        )}
+        {statusData.completed ? (
+          <p>Completed!</p>
+        ) : (
+          <p>Not completed.</p>
+        )}
+        <p>Messages:</p>
+        {statusData.message !== undefined ? (
+          <ul>
+            {statusData.message.map((message, index) => (
+              <li key={index}>{message}</li>
+            ))}
+          </ul>
+        ) : (
+          <p>N/A</p>
+        )}
+        
+      </div>
       <div className="results-section">
-        <p>MLCM Time: {results.mlcmTime}</p>
-        <p>ASTRA Time: {results.astraTime}</p>
-        <p>MIRT Time: {results.mirtTime}</p>
-        <p>TIGRE Time: {results.tigreTime}</p>
+        <b>Calculation Time (ms)</b>
+        <p>MLCM: {results.mlcm_time}</p>
+        <p>ASTRA CPU: {results.astra_cpu_time}</p>
+        <p>ASTRA GPU: {results.astra_gpu_time}</p>
+        <p>TIGRE: {results.tigre_time}</p>
       </div>
       <div className='buttons'>
         <button type="submit" onClick={handleCalculate} className='submit_btns'>Calculate</button>
